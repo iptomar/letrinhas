cff6cf42f4e3d4a9f3ad7fc0f15be1b771340f33
ThiagoJVLF <thiago_f@aeiou.pt>
2014-04-14 01:36:33 +0100

    reparacao de error devido à sincronização do projeto

--- Letrinhas02/src/com/letrinhas02/SSID.java
+++ Letrinhas02/src/com/letrinhas02/SSID.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SSID extends Activity {
 	TextView tvSsid,tvPass;
