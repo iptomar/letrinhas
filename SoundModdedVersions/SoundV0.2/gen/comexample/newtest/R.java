032aa9aa9a7d2b8dca6ff382d90cbbf740294970
dariojorge <jorge12dario12@hotmail.com>
2014-04-19 20:26:28 +0100

    corrigir um engano que fiz

--- SoundModdedVersions/SoundV0.2/gen/comexample/newtest/R.java
+++ SoundModdedVersions/SoundV0.2/gen/comexample/newtest/R.java
@@ -34,11 +34,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_mainn=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
-        public static final int mainn=0x7f070001;
     }
     public static final class string {
         public static final int Recording=0x7f050003;
